893026bc45958a8bf0df5855cb4a4fb282842039
HENRIQUE RODRIGUES DA CUNHA <henrique.rcunha1@senacsp.edu.br>
2019-07-30 09:18:08 -0300

    While

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 1;
+
+        //realizar uma contagem de 1 a 10 #endregion
+        while(n <= 10){
+            Console.WriteLine("Estou em "+n);
+            n++;
+
+        }
+
+        }
+    }
+}
